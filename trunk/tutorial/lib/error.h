6767f0ad1c8133d02fe71e1f06c74ff0a12d89eb
edskodevries <edskodevries@e7869359-c92b-0410-9105-cbbd88f55910>
2007-09-25 09:27:08 +0000

    started tutorial

---
+++ trunk/tutorial/lib/error.h
@@ -0,0 +1,30 @@
+/*
+ * phc -- the open source PHP compiler
+ * See doc/license/README.license for licensing information
+ * 
+ * Error handling 
+ */
+
+#ifndef PHC_ERROR
+#define PHC_ERROR
+
+#include "lib/String.h"
+
+class AST_node;
+
+void phc_internal_error (const char* message, String* filename, int line, ...);
+void phc_internal_error (const char* message, AST_node* node, ...);
+void phc_internal_error (const char* message, ...);
+
+
+void phc_error (const char* message, String* filename, int line, ...);
+void phc_error (const char* message, AST_node* node, ...);
+void phc_error (const char* message, ...);
+
+
+void phc_warning (const char* message, String* filename, int line, ...);
+void phc_warning (const char* message, AST_node* node, ...);
+void phc_warning (const char* message, ...);
+
+
+#endif
